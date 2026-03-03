3dfcb3c15e185620b6f432f07581621830cf11d2
giltamano <tamanogil@gmail.com>
2026-03-04 06:09:13 +0800

    completed random qoute generator

---
+++ projectRepos/randomQoutes/script.js
@@ -0,0 +1,28 @@
+const qoutes = [
+    "The best way to predict the future is to create it.",
+    "Sometimes, when things are falling apart, they may actually be falling into place.",
+    "You can't start the next chapter of your life if you keep re-reading your last one.",
+    "To be interesting-be interested.",
+    "In three words I can sum up everything I've learned about life: it goes on."
+]
+
+const usedIndexes = new Set()
+const quoteElement = document.getElementById("qoute")
+
+function generateQoute(){
+    if(usedIndexes.size >= (qoutes.length)){
+            usedIndexes.clear()
+    }
+    while(true){
+
+    const randomIndex = Math.floor(Math.random() * qoutes.length)
+
+        if(usedIndexes.has(randomIndex)) continue
+
+    const quote = qoutes[randomIndex]
+    quoteElement.innerHTML = quote
+    usedIndexes.add(randomIndex)
+    console.log(randomIndex)
+    break
+}
+}
