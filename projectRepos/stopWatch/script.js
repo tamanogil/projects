34c5aea0d0a5a5743a55cd043b3b233c6a489a7d
giltamano <tamanogil@gmail.com>
2026-03-17 13:47:35 +0800

    project completed successfully

--- projectRepos/stopWatch/script.js
+++ projectRepos/stopWatch/script.js
@@ -2,10 +2,14 @@ let secondsElapsed = 0
 let interval = null
 const time = document.getElementById('time')
 
+function padStart(value){
+    return String(value).padStart(2, "0")
+}
+
 function setTime(){
     const minutes = Math.floor(secondsElapsed / 60)
     const seconds = secondsElapsed % 60
-    time.innerHTML = `${minutes}:${seconds}`
+    time.innerHTML = `${padStart(minutes)}:${padStart(seconds)}`
 }
 
 function timer(){
@@ -14,17 +18,18 @@ function timer(){
 }
 
 function startClock(){
+    if(interval)stopClock()
     interval = setInterval(timer, 1000)
+console.log(interval)
 }
 
 function stopClock(){
-    clearInterval(interval)
-    interval = setTimeout(timer, 1000);
+   clearInterval(interval);
+   console.log(interval)
 }
 
 function reset(){
     stopClock()
     secondsElapsed = 0
-    time.innerHTML = secondsElapsed
-    stopClock()
+    setTime()
 }
