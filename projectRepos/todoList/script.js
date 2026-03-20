b839baf5d6e9981bc2c6f30876cddd99d0215fe2
giltamano <tamanogil@gmail.com>
2026-03-20 12:49:56 +0800

    updated todo list

--- projectRepos/todoList/script.js
+++ projectRepos/todoList/script.js
@@ -1,11 +1,30 @@
-let item = []
+let items = []
+const itemDiv = document.getElementById("items")
+const input = document.getElementById('todo')
 
 function loadItems(){}
 
-function renderItems(){}
+function renderItems(){
+     itemDiv.innerHTML = null
+
+    for (const [idx, item] of Object.entries(items)) {
+        const text = document.createElement('p')
+        text.textContent = item
+    
+    const container = document.createElement('div')
+
+    itemDiv.appendChild(text)
+    itemDiv.appendChild(container)
+}
+}
+
 
 function saveItems(){}
 
-function addItem() {}
+function addItem() {
+    const todo = input.value
+    items.push(todo)
+   renderItems()
+}
 
 function removeItems(){}
