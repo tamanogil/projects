27458b76350f48c5478604939c3c787995b6a922
giltamano <tamanogil@gmail.com>
2026-03-21 14:09:00 +0800

    updated todo list

---
+++ projectRepos/todoList/script.js
@@ -0,0 +1,32 @@
+let items = []
+const itemDiv = document.getElementById("items")
+let inputValue = document.getElementById('input')
+
+
+function renderItems(){
+    itemDiv.innerHTML = ''
+    for (const [idx, item] of Object.entries(items)) {
+    let container = document.createElement('div')
+    const text = document.createElement('p')
+    //create p element to show items on div
+    text.textContent = item
+    item.onclick = () => removeItem(idx)
+    //set to display p value to the input element
+
+    //create specific divs for each item element
+    itemDiv.appendChild(text)
+    itemDiv.appendChild(container)
+    
+    //set items div to an empty string
+    }
+}
+renderItems()
+function addItem(){
+    const value = inputValue.value
+    items.push(value)
+    renderItems()
+}
+function removeItem(idx){
+    items.splice(idx , 1)
+    renderItems()
+}
