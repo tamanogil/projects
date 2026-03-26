60e2c007b6819f9da80bf9eb542e52e755036cbc
giltamano <tamanogil@gmail.com>
2026-03-27 03:26:04 +0800

    project successfully completed

--- projectRepos/randomPassword/script.js
+++ projectRepos/randomPassword/script.js
@@ -3,10 +3,15 @@ const inputValue = document.getElementById('text')
 let randomStr = Math.random().toString(36).substring(2,12)
 
 
-function generatePassword(){
-    inputValue.value = randomStr
-    if(randomStr != "")
-     randomStr = Math.random().toString(36).substring(2,12)
+function generatePassword() {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789! @#$%^&*()_-+={[}]|\:;"<,>.?/';
+  let result = '';
+  for (let i = 0; i < 10; i++) {
+    // Select a random character from the defined set
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters.charAt(randomIndex);
+  }
+  inputValue.value = result
 }
 function copy(){
     navigator.clipboard.writeText(inputValue.value)
