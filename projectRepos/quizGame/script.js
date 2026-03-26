b1b54e99bee2b0966b50d08208b68e2e2cd38580
giltamano <tamanogil@gmail.com>
2026-03-26 21:19:52 +0800

    updated quiz game

--- projectRepos/quizGame/script.js
+++ projectRepos/quizGame/script.js
@@ -1,16 +1,113 @@
-const resultElement = document.getElementById('result')
-const selectedOption = document.querySelector('input[name="color"]:checked')
-let score = 0
+const questions = [
+    {
+        question: "Which is largest animal in the world?",
+        answers: [
+            { text: "Shark", correct: false},
+            { text: "Blue whale", correct: true},
+            { text: "Elephant", correct: false},
+            { text: "Giraffe", correct: false},
+        ]
+    },
+    {
+        question: "Which is the smallest country in the world?",
+        answers: [
+            { text: "Vatican City", correct: true},
+            { text: "Bhutan", correct: false},
+            { text: "Nepal", correct: false},
+            { text: "Shri Lanka", correct: false},
+        ]
+    },
+    {
+        question: "Which is the largest desert in the world?",
+        answers: [
+            { text: "Kalahari", correct: false},
+            { text: "Gobi", correct: false},
+            { text: "Sahara", correct: false},
+            { text: "Antarctica", correct: true},
+        ]
+    },
+    {
+        question: "Which is the smallest continent in the world?",
+        answers: [
+            { text: "Asia", correct: false},
+            { text: "Australia", correct: true},
+            { text: "Arctic", correct: false},
+            { text: "Africa", correct: false},
+        ]
+    }  
+];
 
-function checkValue(){
-    const selectedOption = document.querySelector('input[name="color"]:checked')
-    let selected =  selectedOption.value
-    if(selected == "milk"){
-        score++
-     resultElement.textContent = "Correct"}
-     else{
-        resultElement.textContent = "Wrong"
-     }
-     console.log(selected)
+const questionElement = document.getElementById("question")
+const answerButtons = document.getElementById("answer-buttons")
+const nextButton = document.getElementById("next-btn")
+
+let currentQuestionIndex = 0
+let score = 0 
+
+function startQuiz(){
+    currentQuestionIndex = 0
+    score = 0 
+    nextButton.innerHTML = 'Next'
+    showQuestion()
 }
 
+function showQuestion(){
+    resetState()
+    let currentQuestion = questions[currentQuestionIndex]
+    let questionNo = currentQuestionIndex + 1
+    questionElement.innerHTML = questionNo + "." + currentQuestion.question
+    
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        answerButtons.appendChild(button)
+        if(answer.correct){
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click' , selectAnswer)
+    })
+}
+function resetState(){
+    nextButton.style.display = "none"
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild)
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target
+    const isCorrect = selectedBtn.dataset.correct == true
+    if(isCorrect) {score++
+    console.log('Correct')}
+    else{
+        console.log('Incorrect')
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        button.disabled = true
+    })
+    nextButton.style.display = 'block'
+}
+function showScore(){
+    resetState()
+    questionElement.innerHTML = `You scored ${score}`
+    nextButton.style.display = 'block';
+}
+function handleNextButton(){
+    currentQuestionIndex++
+    if(currentQuestionIndex < questions.length){
+        showQuestion()
+    }
+    else{
+        showScore()
+    }
+}
+nextButton.addEventListener('click', () => {
+    if(currentQuestionIndex < questions.length){
+        handleNextButton()
+    }
+    else{
+        startQuiz()
+    }
+})
+startQuiz()``
