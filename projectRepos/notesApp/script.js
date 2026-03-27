b4846bf28edeb595bd2c25d23a64668bb45817ff
giltamano <tamanogil@gmail.com>
2026-03-27 15:06:12 +0800

    updated saving on local storage functionality

--- projectRepos/notesApp/script.js
+++ projectRepos/notesApp/script.js
@@ -1,6 +1,6 @@
 let items = []
 const divElement = document.getElementById('emptyDiv')
-let storageKey = ""
+let storageKey = "notes"
 
 function createNote(){
     divElement.innerHTML = ""
@@ -15,13 +15,15 @@ editableDiv.contentEditable = "true";
 editableDiv.style.width = "300px";
 editableDiv.style.height = "100px";
 editableDiv.style.border = "1px solid black";
-editableDiv.style.padding = "10px";
-editableDiv.innerText = `Add notes here....`
-console.log(items)
-editableDiv.addEventListener('click', () =>{
-    editableDiv.innerText = ""
-    }
-)
+editableDiv.innerText = item || "add note here"
+editableDiv.addEventListener('click' , () => {
+    editableDiv.innerText = "" 
+}, { once: true })
+editableDiv.style.padding = "10px"; 
+editableDiv.oninput = () => {
+    items[idx] = editableDiv.innerText
+    saveItems()
+}
 deleteBtn.textContent = "Delete"
 deleteBtn.onclick = () => removeItem(idx)
 // 4. Append to the document
@@ -41,4 +43,16 @@ function removeItem(idx){
     items.splice(idx,1)
     createNote()
 }
+function saveItems(){
+    const stringifyItems = JSON.stringify(items)
+    localStorage.setItem(storageKey, stringifyItems)
+}
+function loadItems(){
+    const originalArray = localStorage.getItem(storageKey)
+    if(originalArray)
+        items = JSON.parse(originalArray)
+    createNote()
+}
+
+document.addEventListener('DOMContentLoaded' , loadItems)
 
