3d8579056fa4ba04eadb1f8feb2ad403294db5e5
giltamano <tamanogil@gmail.com>
2026-04-06 13:18:54 +0800

    project completed successfully

--- projectRepos/studentManagement/script.js
+++ projectRepos/studentManagement/script.js
@@ -7,11 +7,14 @@ class Student {
         this.hobbies = hobbies
     }
 }
-
+let studentsArr = JSON.parse(localStorage.getItem('students')) || []
 const addStudentBtnEl = document.getElementById('addStudent')
 const showStudentBtnEl = document.getElementById('showStudent')
 const textDisplayEl = document.getElementById('textDisplay')
 const recordDisplayEl = document.getElementById('recordDisplay')
+window.onload = () => {
+    studentsArr.forEach((e => renderItems(e)))
+}
 
 
 addStudentBtnEl.addEventListener('click',  addStudent)
@@ -22,20 +25,45 @@ recordDisplayEl.style.display = 'none'
 function addStudent(){
     
     let userId = prompt('Enter ID')
+     if(userId == null || userId.trim() == "" || isNaN(parseInt(userId))){
+        alert("You've entered an invalid ID.")
+        return
+    }
     let userName = prompt('Enter Name')
+    if(userName == null || userName.trim() == "" ){
+        alert("You've entered an invalid name.")
+        return
+    }
     let userAge = prompt('Enter Age')
+    if(userAge == null || userAge.trim() == "" || isNaN(parseInt(userAge))){
+        alert("You've entered an invalid age.")
+        return
+    }
     let userGrade = prompt('Enter Grade')
+    if(userGrade == null || userGrade.trim() == "" || isNaN(parseInt(userGrade))){
+        alert("You've entered an invalid grade.")
+        return
+    }
     let userHobby = prompt('Enter Hobbies')
-
+    if(userHobby == null || userHobby.trim() == ""){
+        alert("You've entered an invalid hobbies")
+        return
+    }
     const newStudent = new Student(userId, userName, userAge,userGrade,
         userHobby)
-    const myDiv = document.createElement("div");
+        
+    studentsArr.push(newStudent)
+    localStorage.setItem("students", JSON.stringify(studentsArr))
+    renderItems(newStudent)
+}
+
+function renderItems(students){
+const myDiv = document.createElement("div");
     recordDisplayEl.appendChild(myDiv);
-    myDiv.textContent = `ID: ${newStudent.id} Name: ${newStudent.name} Age: ${newStudent.age} Grade: ${newStudent.grade}
-    Hobbies: ${newStudent.hobbies}`
+    myDiv.textContent = `ID: ${students.id} | Name: ${students.name} | Age: ${students.age} | Grade: ${students.grade} |
+    Hobbies: ${students.hobbies}`
 }
 
 function showStudent(){
-    recordDisplayEl.style.display = 'block'
+    recordDisplayEl.style.display = recordDisplayEl.style.display === 'none' ? 'block' : 'none'  
 }
-
