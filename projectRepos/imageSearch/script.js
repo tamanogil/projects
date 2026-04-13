3a0816c842b9b2df0e648add723ed6308f7e4cea
giltamano <tamanogil@gmail.com>
2026-04-13 16:24:13 +0800

    updated image search app

--- projectRepos/imageSearch/script.js
+++ projectRepos/imageSearch/script.js
@@ -1,15 +1,33 @@
 const searchFormEl = document.getElementById("searchForm")
+const imageDisplayEl = document.getElementById("imageDisplay")
+const searchBoxEl = document.getElementById('searchBox')
+const accessKey = ""
 
+let keyword = ""
+let page = 1
 
 async function searchImages(){
-    const url = `https://api.unsplash.com/search/photos?page=${page}&
-    query=${keyword}&client_id=${accessKey}`
+    keyword = searchBoxEl.value
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`
 
     const response = await fetch(url)
     const data = await response.json()
+    const results = data.results
+
+    results.map((e) => {
+        const image = document.createElement('img');
+        image.src = e.urls.small
+        const imageLink = document.createElement('a');
+        imageLink.href = e.links.html;
+        imageLink.target = "_blank"
+
+        imageLink.appendChild(image)
+        imageDisplayEl.appendChild(imageLink)
+    })
 }
 
 searchFormEl.addEventListener("submit", (e) => {
     e.preventDefault();
+    page = 1
     searchImages()
 })
