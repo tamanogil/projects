a2c997293b7013e63609156d4893e7fb58da3702
giltamano <tamanogil@gmail.com>
2026-04-15 06:11:15 +0800

    project completed successfully

--- projectRepos/formValidation/script.js
+++ projectRepos/formValidation/script.js
@@ -2,6 +2,7 @@ let nameErrorEl = document.getElementById("nameError")
 let phoneErrorEl = document.getElementById("phoneError")
 let emailErrorEl = document.getElementById("emailError")
 let messageErrorEl = document.getElementById("messageError")
+let validateEl = document.getElementById("validate")
 
 function handleName(){
     const name = document.getElementById("name").value
@@ -17,4 +18,51 @@ function handleName(){
     nameErrorEl.innerText = "✅"
     return true
 }
+function handlePhone(){
+    const phone = document.getElementById('number').value
 
+    if(phone.length < 11){
+        phoneErrorEl.innerText = "Phone number is required"
+        return false
+    }
+    if(isNaN(parseInt(phone))){
+        phoneErrorEl.innerText = "Invalid phone number"
+        return false
+    }
+    phoneErrorEl.innerText = "✅"
+    return true
+}
+function handleEmail(){
+    const email = document.getElementById('email').value
+
+    if(email.length < 8){
+        emailErrorEl.innerText = "Email is required"
+        return false
+    }
+    if(!email.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)){
+        emailErrorEl.innerText = "Invalid email format"
+        return false
+    }
+    emailErrorEl.innerText = "✅"
+    return true
+}
+function handleMessage(){
+    const message = document.getElementById('message').value
+
+    if(message.length < 3){
+        messageErrorEl.innerText = "Message is required"
+        return false
+    }
+    messageErrorEl.innerText = "✅"
+    return true
+}
+function validateForm(){
+    if(!handleName() || !handlePhone() 
+        || !handleEmail() || !handleMessage()){
+    validateEl.style.display = "block"
+    validateEl.textContent = "Please fix errors"
+    setTimeout(function(){
+        validateEl.style.display = "none"} , 3000)
+        return false
+    }
+}
