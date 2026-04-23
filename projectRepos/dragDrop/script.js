5f851d52a4ec087d6f0ecaba16ac2bdc6936ba10
giltamano <tamanogil@gmail.com>
2026-04-23 20:01:30 +0800

    added script file

---
+++ projectRepos/dragDrop/script.js
@@ -0,0 +1,17 @@
+let lists = document.getElementsByClassName('list')
+let rightBox = document.getElementById('right')
+let leftBox = document.getElementById('left')
+
+for(list of lists){
+    lists.addEventListener('dragstart', function(e){
+        let selected = e.target
+
+        rightBox.addEventListener('dragover', function(e){
+            e.preventDefault()
+        })
+        rightBox.addEventListener('drop', function(e){
+            rightBox.appendChild(selected)
+            selected = null
+        })
+    })
+}
